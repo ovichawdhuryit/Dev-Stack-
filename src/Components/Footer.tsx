e56e064f08bbef25e980a79898708d2295a4c8d0
Anik Chawdhury <ovichawdhuryit@gmail.com>
2026-09-13 03:21:36 +0600

    footer section complete

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,11 +1,11 @@
 export default function Footer() {
     return (
-        <footer>
-            <div className="flex justify-between ">
+        <footer className="border-t border-gray-200 px-6 py-12">
+            <div className="mx-auto max-w-7xl flex  justify-between ">
                 <div>
                     <img src="/assets/logo-text.png" alt="footer logo" />
 
-                    <p className="py-1.5">Curated tools, technologies, and resources for developers building <br /> modern software.</p>
+                    <p className=" text-sm text-gray-600">Curated tools, technologies, and resources for developers building <br /> modern software.</p>
 
                     <ul className="flex gap-2.75">
                         <li> GitHub</li>
@@ -15,8 +15,8 @@ export default function Footer() {
                 </div>
 
                 <div>
-                    <h3>PRODUCT</h3>
-                    <ul className="py-1.5">
+                    <h3 className="mb-4 text-sm font-semibold">PRODUCT</h3>
+                    <ul className="space-y-3 text-sm text-gray-600">
                         <li>Home</li>
                         <li>Technologies</li>
                         <li>Projects</li>
@@ -24,8 +24,8 @@ export default function Footer() {
                 </div>
 
                 <div>
-                    <h3>COMPANY</h3>
-                    <ul className="py-1.5">
+                    <h3 className="mb-4 text-sm font-semibold">COMPANY</h3>
+                    <ul className="space-y-3 text-sm text-gray-600">
                         <li>About</li>
                         <li>Contact</li>
                         <li>Career</li>
@@ -33,11 +33,21 @@ export default function Footer() {
                 </div>
 
                 <div>
-                    <h3>LEGAL</h3>
-                    <ul className="py-1.5">
+                    <h3 className="mb-4 text-sm font-semibold">LEGAL</h3>
+                    <ul className="space-y-3 text-sm text-gray-600">
                         <li> Privacy Policy</li>
                         <li>Terms of Service</li>
                     </ul>
+                </div>
+            </div>
+            <div className="mx-auto mt-10 max-w-7xl border-t border-gray-200 pt-6">
+                <div className="flex justify-between text-sm text-gray-500">
+                    <p>© 2026 Dev Stack. All rights reserved.</p>
+
+                    <div className="flex gap-6">
+                        <a href="">Privacy Policy</a>
+                        <a href="">Terms of Service</a>
+                    </div>
                 </div>
             </div>
         </footer>
