b349af467e0c3bdb6765ba31af810073bfdad21a
Anik Chawdhury <ovichawdhuryit@gmail.com>
2026-09-13 16:11:56 +0600

    Toastify Added

--- src/App.tsx
+++ src/App.tsx
@@ -5,28 +5,39 @@ import Navbar from "./Components/Navbar";
 import Stack from "./Components/Stack";
 import type { Technology } from "./Types/Types";
 import { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 
 
 function App() {
   const [selectedTechnologies, setSelectedTechnologies] = useState<Technology[]>([]);
-
   const addToStack = (technology: Technology) => {
     if (selectedTechnologies.some((item) => item.id === technology.id)) {
+      toast.warning(`${technology.name} is already in your stack!`);
       return;
     }
 
     setSelectedTechnologies([...selectedTechnologies, technology]);
+    toast.success(`${technology.name} added to your stack!`);
   };
 
   const removeFromStack = (id: number) => {
+    const technology = selectedTechnologies.find((item) => item.id === id);
+
     setSelectedTechnologies(
       selectedTechnologies.filter((item) => item.id !== id)
     );
+
+    if (technology) {
+      toast.info(`${technology.name} removed from your stack!`);
+    }
   };
 
   const removeAll = () => {
     setSelectedTechnologies([]);
+
+    toast.info("All technologies removed from your stack!");
   };
 
 
@@ -51,6 +62,7 @@ function App() {
           />
         </div>
       </main>
+      <ToastContainer />
 
       <Footer />
     </>
